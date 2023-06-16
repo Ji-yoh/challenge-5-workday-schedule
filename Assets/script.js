77e68b6e385e8b43221a71a9e3edb30554d836fe
Ji-yoh <geovannih514@gmail.com>
2023-06-15 19:49:28 -0700

    add event listener & save to local storage (1st todo)

--- Assets/script.js
+++ Assets/script.js
@@ -8,16 +8,33 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  // 1st todo: Saved schedule items to local storage
+    $(".saveBtn").on("click", function () {
+      var time = $(this).parent().attr("id");
+      var text = $(this).siblings(".description").val();
+      console.log(time, text);
+      var savedItems = JSON.parse(localStorage.getItem("savedItems"));
+      if (!savedItems) {
+        savedItems = [];
+      };
+
+      savedItems.push({ time, text });
+
+      localStorage.setItem("savedItems", JSON.stringify(savedItems));
+    })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  // 2nd todo: Color code time blocks based on time of day
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  // 3rd todo: Retrieve saved schedule items from local storage
+
   // TODO: Add code to display the current date in the header of the page.
+  // 4th todo: Display current date in header, use Day.js
 });

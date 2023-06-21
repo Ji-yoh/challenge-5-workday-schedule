bbb43006023c0dc84a41d75d47644de2cde85115
Ji-yoh <geovannih514@gmail.com>
2023-06-20 17:47:11 -0700

    schedule items now saved to and retrieved from local storage

--- Assets/script.js
+++ Assets/script.js
@@ -13,14 +13,16 @@ $(function () {
       var time = $(this).parent().attr("id");
       var text = $(this).siblings(".description").val();
       console.log(time, text);
-      var savedItems = JSON.parse(localStorage.getItem("savedItems"));
+      localStorage.setItem(time, text);
+      // originally saved time and text to local storage as an object called savedItems, but was unable to retrieve the object from local storage. saved time and text as individual items instead.
+      /* var savedItems = JSON.parse(localStorage.getItem("savedItems"));
       if (!savedItems) {
         savedItems = [];
       };
 
       savedItems.push({ time, text });
 
-      localStorage.setItem("savedItems", JSON.stringify(savedItems));
+      localStorage.setItem("savedItems", JSON.stringify(savedItems)); */
     })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -29,11 +31,11 @@ $(function () {
   // current hour in 24-hour time?
   // 2nd todo: Color code time blocks based on time of day
     var currentHour = dayjs().hour();
-    console.log(currentHour);
+    
     $(".time-block").each(function () {
   
       var scheduleTime = parseInt($(this).attr("id"));
-      console.log(scheduleTime);
+      
       if (currentHour > scheduleTime) { // 'this' was used instead of calling the class name .time-block, originally was causing future class to be applied to all time blocks
         $(this).addClass("past");$(this).removeClass("present"); 
         $(this).removeClass("future");
@@ -47,13 +49,23 @@ $(function () {
         $(this).removeClass("present");
       } 
     });
-    
+  
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   // 3rd todo: Retrieve saved schedule items from local storage
-
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
+    console.log(localStorage.getItem("17"));
   // TODO: Add code to display the current date in the header of the page.
   // 4th todo: Display current date in header, use Day.js
+  
 });
